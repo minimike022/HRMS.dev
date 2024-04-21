496b4bff12f3ed86f0f38c294dc8e741b13e24b7
MiniMike <minimike156@gmail.com>
2024-04-21 19:34:34 +0800

    Adding quickfix

--- app/routes/handler.go
+++ app/routes/handler.go
@@ -1,13 +1,13 @@
 package route
 
 import (
-	routesData "hrms-api/app/service"
+	service "hrms-api/app/service"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupRoutes(app *fiber.App) {
-	app.Get("/", routesData.GetData)
-	app.Post("/postData", routesData.PostData)
+	app.Get("/", service.GetData)
+	app.Post("/postData", service.PostData)
 	//app.Get("/putData",routesData.PutData)
 }
 
--- app/service/service.go
+++ app/service/service.go
@@ -1,4 +1,4 @@
-package routesData
+package service
 
 import (
 	"fmt"
